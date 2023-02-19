36e9d1e8cc12ef09fd11a0fba1fe5f44e0eb89d9
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-02-19 18:38:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,4 @@ module.exports = {
        MONGO_URL : "mongodb+srv://afx-ToxicLeo:afx-ToxicLeo@cluster0.oukc2ds.mongodb.net/?retryWrites=true&w=majority",//enter your mongodb url to get and store youe session!
        SESSION_PREFIX : 'leo~'// defult value of the satrting of your session
 }
-/*
+*/
